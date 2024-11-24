acfe9bbdf9002593f5b2ef738dc3a3a5a7da3725
anwrazizz <anwar23ti@mahasiswa.pcr.ac.id>
2024-11-24 15:10:08 +0700

    Anwar Aziz

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ Route::get('/', function () {
     return view('SideBar/navbar');
 });
 
-Auth::routes();
-
 Route::middleware([Authenticate::class])->group(function () {
     Route::resource('siswa', SiswaController::class);
     // Route::resource('daftar', DaftarController::class);
