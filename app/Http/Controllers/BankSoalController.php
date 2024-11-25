7dafe969adcad71038cadbd2cd09f42cad2dc24a
angky23ti <angky23ti@mahasiswa.pcr.ac.id>
2024-11-25 09:58:16 +0700

    tambah file MVC

---
+++ app/Http/Controllers/BankSoalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\bankSoal;
+use App\Http\Requests\StorebankSoalRequest;
+use App\Http\Requests\UpdatebankSoalRequest;
+
+class BankSoalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorebankSoalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(bankSoal $bankSoal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(bankSoal $bankSoal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatebankSoalRequest $request, bankSoal $bankSoal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(bankSoal $bankSoal)
+    {
+        //
+    }
+}
