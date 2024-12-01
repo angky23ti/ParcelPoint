e1f8c149e26ebcb0a8a79888ed2620ea3a874a27
anwrazizz <anwar23ti@mahasiswa.pcr.ac.id>
2024-12-01 23:26:38 +0700

    update menu ujian

---
+++ resources/views/Ujian/create.blade.php
@@ -0,0 +1,104 @@
+@extends('SideBar.navbar', ['title' => 'Tambah Data Siswa'])
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <center>
+                <h5 class="card-header">Tambah Data Siswa</h5>
+            </center>
+            <form action="/siswa" method="POST" enctype="multipart/form-data">
+                @csrf
+                <!-- Input NISN -->
+                <div class="form-group mt-1 mb-3">
+                    <label for="nisn"><b>NISN</b></label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-primary text-white">
+                                <i class="bi bi-person"></i>
+                            </span>
+                        </div>
+                        <input type="text"
+                               class="form-control @error('nisn') is-invalid @enderror"
+                               id="nisn" name="nisn" placeholder="Masukkan NISN" value="{{ old('nisn') }}">
+                    </div>
+                    <span class="text-danger">{{ $errors->first('nisn') }}</span>
+                </div>
+
+                <!-- Input Nama -->
+                <div class="form-group mt-1 mb-3">
+                    <label for="nama"><b>Nama</b></label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-primary text-white">
+                                <i class="bi bi-person"></i>
+                            </span>
+                        </div>
+                        <input type="text"
+                               class="form-control @error('nama') is-invalid @enderror"
+                               id="nama" name="nama" placeholder="Masukkan nama lengkap" value="{{ old('nama') }}">
+                    </div>
+                    <span class="text-danger">{{ $errors->first('nama') }}</span>
+                </div>
+
+                <!-- Input Kelas -->
+                <div class="form-group mt-1 mb-3">
+                    <label for="kelas"><b>Kelas</b></label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-primary text-white">
+                                <i class="bi bi-building"></i>
+                            </span>
+                        </div>
+                        <input type="text" class="form-control @error('kelas') is-invalid @enderror"
+                               id="kelas" name="kelas" placeholder="Masukkan kelas" value="{{ old('kelas') }}">
+                    </div>
+                    <span class="text-danger">{{ $errors->first('kelas') }}</span>
+                </div>
+
+                <!-- Input Username -->
+                <div class="form-group mt-1 mb-3">
+                    <label for="username"><b>Username</b></label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-primary text-white">
+                                <i class="bi bi-person-circle"></i>
+                            </span>
+                        </div>
+                        <input type="text" class="form-control @error('username') is-invalid @enderror" id="username" name="username"
+                               placeholder="Masukkan username" value="{{ old('username') }}">
+                    </div>
+                    <span class="text-danger">{{ $errors->first('username') }}</span>
+                </div>
+
+                <!-- Input Password -->
+                <div class="form-group mt-1 mb-3">
+                    <label for="password"><b>Password</b></label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-primary text-white">
+                                <i class="bi bi-lock"></i>
+                            </span>
+                        </div>
+                        <input type="password" class="form-control @error('password') is-invalid @enderror"
+                            id="password" name="password" placeholder="Masukkan password">
+                    </div>
+                    <span class="text-danger">{{ $errors->first('password') }}</span>
+                </div>
+
+                <!-- Input Foto -->
+                <div class="form-group mt-1 mb-3">
+                    <label for="foto"><b>Foto</b></label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-primary text-white">
+                                <i class="bi bi-camera"></i>
+                            </span>
+                        </div>
+                        <input type="file" class="form-control @error('foto') is-invalid @enderror" id="foto" name="foto">
+                    </div>
+                    <span class="text-danger">{{ $errors->first('foto') }}</span>
+                </div>
+                <button type="submit" class="btn btn-primary w-100">SIMPAN</button>
+            </form>
+        </div>
+    </div>
+@endsection
--- resources/views/Ujian/index.blade.php
+++ resources/views/Ujian/index.blade.php
@@ -1,47 +1,42 @@
-@extends('SideBar.navbar', ['title' => 'Data Siswa'])
+@extends('SideBar.navbar', ['title' => 'Data Ujian'])
 
 @section('content')
     <div class="card">
         <center>
-            <h3 class="card-header">Data Siswa</h3>
+            <h3 class="card-header">Data Ujian</h3>
         </center>
         <div class="card-body">
             <div class="row mb-3 mt-3">
-                <!-- Tombol tambah siswa -->
+                <!-- Tombol tambah Ujian -->
                 <div class="col-md-6">
-                    <a href="{{ route('siswa.create') }}" class="btn btn-primary btn-sm">Tambah Siswa</a>
+                    <a href="{{ route('Ujian.create') }}" class="btn btn-primary btn-sm">Tambah Guru</a>
                 </div>
             </div>
 
             <table class="table table-striped">
                 <thead>
                     <tr>
-                        <th>No</th>
-                        <th>NISN</th>
-                        <th>Nama</th>
+                        <th>Kode Ujian</th>
+                        <th>Mata Pelajaran</th>
+                        <th>Tanggal Mulai</th>
+                        <th>Tanggal Akhir</th>
                         <th>Kelas</th>
-                        <th>Username</th>
-                        <th>Password</th>
+                        <th>Kode Token</th>
+                        <th>Status</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
                     <!-- Cek jika ada data -->
-                    @forelse ($siswa as $item)
+                    @forelse ($ujian as $item)
                         <tr>
                             <td>{{ $loop->iteration }}</td>
-                            <td>{{ $item->nisn }}</td>
-                            <td>
-                                @if($item->foto)
-                                    <a href="{{ \Storage::url($item->foto) }}" target="_blank">
-                                        <img src="{{ \Storage::url($item->foto) }}" style="max-width: 50px; height: auto;" />
-                                    </a>
-                                @endif
-                                {{ $item->nama }}
-                            </td>
+                            <td>{{ $item->kode}}</td>
+                            <td>{{ $item->mapel }}</td>
+                            <td>{{ $item->mulai }}</td>
+                            <td>{{ $item->akhir }}</td>
                             <td>{{ $item->kelas }}</td>
-                            <td>{{ $item->username }}</td>
-                            <td>{{ $item->password }}</td>
+                            <td>{{ $item->status }}</td>
                             <td>
                                 <a href="/siswa/{{ $item->id }}/edit" class="btn btn-warning btn-sm m1-2">Edit</a>
                                 <form action="/siswa/{{ $item->id }}" method="POST" class="d-inline">
